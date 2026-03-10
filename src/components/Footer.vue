ade310c869fd95164edc739e14e9a6dc06106200
pedromeixu <pedromeixu44@gmail.com>
2026-03-10 04:28:56 +0100

    css classes added

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,14 @@
-<script setup>
-</script>
-
 <template>
-    <footer>
-        <div>
-            <p>Footer</p>
-        </div>
+    <footer class="footer">
+        <p>Footer</p>
     </footer>
 </template>
+
+<style scoped>
+    .footer {
+        background: #222;
+        color: white;
+        text-align: center;
+        padding: 1rem;
+    }
+</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,23 +1,33 @@
 <template>
-    <nav>
-        <div>
-            <ul>
-                <li>
-                    <router-link to="/" class="nav-link">Inicio</router-link>
-                </li>
-                <li>
-                    <router-link to="/empleados" class="nav-link">Empleados</router-link>
-                </li>
-                <li>
-                    <router-link to="/tareas" class="nav-link">Tareas</router-link>
-                </li>
-                <li>
-                    <router-link to="/estadisticas" class="nav-link">Estadísticas</router-link>
-                </li>
-                <li>
-                    <router-link to="/about" class="nav-link">Sobre nosotros</router-link>
-                </li>
-            </ul>
-        </div>
+    <nav class="navbar">
+        <ul>
+            <li><router-link to="/" class="nav-link">Inicio</router-link></li>
+            <li><router-link to="/empleados" class="nav-link">Empleados</router-link></li>
+            <li><router-link to="/tareas" class="nav-link">Tareas</router-link></li>
+            <li><router-link to="/estadisticas" class="nav-link">Estadísticas</router-link></li>
+            <li><router-link to="/about" class="nav-link">Sobre nosotros</router-link></li>
+        </ul>
     </nav>
 </template>
+<style scoped>
+    .navbar {
+        background: #222;
+        padding: 1rem;
+    }
+
+    ul {
+        display: flex;
+        gap: 1rem;
+        list-style: none;
+    }
+
+    a {
+        color: white;
+        text-decoration: none;
+    }
+
+    a.router-link-active {
+        font-weight: bold;
+        text-decoration: underline;
+    }
+</style>
